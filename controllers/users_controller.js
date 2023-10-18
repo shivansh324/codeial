2f98b44fad6b515c180d3640278d27e1a13fff99
Shivansh <shivansha56@gmail.com>
2023-10-19 00:11:10 +0530

    added authentication via passport.js, express-session, MongoStore

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,12 +8,20 @@ module.exports.profile=function(req,res){
 
 //render the sign up page
 module.exports.signUp=function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+
     return res.render('user_sign_up',{
         title: "Codial | Sign Up"
     })
 }
 //render the sign in page
 module.exports.signIn=function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title: "Codial | Sign In"
     })
@@ -34,5 +42,12 @@ module.exports.create = function(req, res){
 }
 //sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    //later
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req,res){
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        return res.redirect('/users/sign-in');
+    });
 }
--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
-const expressLayouts=require('express-ejs-layouts');
-const db= require('./config/mongoose');
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
 
 //reading through post requests
 app.use(express.urlencoded());
@@ -16,15 +21,42 @@ app.use(express.static('./assets'));
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-
 app.use(expressLayouts);
-
-//use exp router
-app.use('/',require('./routes'));
 
 //set up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
+//mongo store is used to store the session  cookie in the db
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+            mongoUrl: 'mongodb://127.0.0.1:27017/codeial_development',
+            // mongooseConnection: db,
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+//use exp router
+app.use('/',require('./routes'));
+
 
 app.listen(port,function(err){
     if(err){
